a404d4c4902f1670a86322bf38d00da44277158b
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-01-25 18:50:13 +0530

    blinkit ui project setup

---
+++ lib/repository/screens/splash/splashscreen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:blinkit_ui/repository/screens/login/loginscreen.dart';
+import 'package:blinkit_ui/repository/widgets/uihelper.dart';
+import 'package:flutter/material.dart';
+import 'package:blinkit_ui/domain/constants/appcolor.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldbackground,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            UiHelper.CustomImage(img: 'img.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
