f393aac839ebde7a72d78efaa3648585d5284583
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-01-26 10:08:39 +0530

    login screen UI

--- lib/repository/screens/login/loginscreen.dart
+++ lib/repository/screens/login/loginscreen.dart
@@ -1,3 +1,4 @@
+import 'package:blinkit_ui/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -6,8 +7,105 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Screen'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          UiHelper.CustomImage(img: 'img_1.png'),
+          SizedBox(
+            height: 10,
+          ),
+          // Image.asset('assets/images/img_2.png',scale: 5,)
+          UiHelper.CustomImage(img: 'img_2.png', scale: 5),
+          SizedBox(
+            height: 10,
+          ),
+          UiHelper.CustomText(
+              text: "India's last minute app",
+              color: Colors.black,
+              fontweight: FontWeight.bold,
+              fontsize: 20,
+              fontfamily: "bold"),
+          SizedBox(
+            height: 20,
+          ),
+          Card(
+            elevation: 4,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Container(
+              height: 200,
+              width: 350,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0XFFFFFFFF)),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  UiHelper.CustomText(
+                      text: 'Saurav Kumar',
+                      color: Colors.black,
+                      fontweight: FontWeight.normal,
+                      fontsize: 14),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  UiHelper.CustomText(
+                      text: '72XXXXXXXX',
+                      color: Colors.black,
+                      fontweight: FontWeight.normal,
+                      fontsize: 14),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 48,
+                    width: 295,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0XFFE23744),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            UiHelper.CustomText(
+                                text: 'Login with',
+                                color: Colors.white,
+                                fontweight: FontWeight.bold,
+                                fontsize: 18,
+                                fontfamily: "bold"),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            UiHelper.CustomText(
+                                text: 'zomato',
+                                color: Colors.white,
+                                fontweight: FontWeight.bold,
+                                fontsize: 22,
+                                fontfamily: "bold"),
+                          ],
+                        )),
+                  ),
+                  UiHelper.CustomText(
+                      text: "Access your saved addresses from zomato automatically",
+                      color: Colors.black54,
+                      fontweight: FontWeight.normal,
+                      fontsize: 10),
+                  SizedBox(height: 15,),
+                  UiHelper.CustomText(
+                      text: 'or login with phone number',
+                      color: Colors.green,
+                      fontweight: FontWeight.bold,
+                      fontsize: 14),
+
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
-class UiHelper{
-  static CustomImage({required String img}){
-    return Image.asset("assets/images/$img");
+class UiHelper {
+  static CustomImage({required String img, double? scale}) {
+    return Image.asset(
+      "assets/images/$img",
+      scale: scale,
+    );
+  }
+
+  static CustomText(
+      {required String text,
+      required Color color,
+      required FontWeight fontweight,
+      String? fontfamily,
+      required double fontsize}) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: fontsize,
+          fontFamily: fontfamily ?? 'regular',
+          fontWeight: fontweight,
+          color: color),
+    );
   }
 }
